58674477e124fcc019706504fbc0b29fefdc59a0
yosukedoke <yd.niku@gmail.com>
2022-08-16 15:15:55 +0900

    ボイラープレートのファイルを削除

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,11 @@
 <script lang="ts">
 import {defineComponent } from 'vue'
-
-import HelloWorld from './components/HelloWorld.vue'
-
-export default defineComponent({
-  components: {
-    HelloWorld,
-  },
-})
+export default defineComponent({})
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main></main>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
