acdba15de96edf646ab41fe03d1e09461708d54a
yosukedoke <yd.niku@gmail.com>
2022-08-16 15:13:31 +0900

    コンポーネントのスタイルをdefineComponentに変更

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,20 @@
-<script setup lang="ts">
-import { ref } from "vue";
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
 
-defineProps<{ msg: string }>();
-
-const count = ref(0);
+export default defineComponent({
+  props: {
+    msg: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {
+    const count = ref(0)
+    return {
+      count,
+    }
+  },
+})
 </script>
 
 <template>
@@ -19,9 +30,8 @@ const count = ref(0);
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
+    starter
   </p>
   <p>
     Install
